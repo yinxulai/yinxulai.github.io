b7d6eb7344a54976dfe79076f8c45bdd38a9175a
yinxulai <yinxulai@hotmail.com>
2021-09-02 09:48:36 +0800

    更新 MAC 屏保

--- web/source/study/2021/08/30/screen-protect.vue
+++ web/source/study/2021/08/30/screen-protect.vue
@@ -4,39 +4,55 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, watch } from 'vue'
-import { useLineSegment } from './use-line-segment'
+import { ref } from 'vue'
+import { makeNoise3D } from 'fast-simplex-noise'
+import { useCanvasRenderer } from '@hooks/use-canvas-renderer'
 
+let zOffset = ref(0.01)
+const angleNoise = makeNoise3D()
+const colorNoise = makeNoise3D()
 const canvasRef = ref<HTMLCanvasElement>()
+const canvasRenderer = useCanvasRenderer(canvasRef, '2d')
 
-const resetSize = (canvas: HTMLCanvasElement) => {
-  if (canvas == null) return
-  const clientRect = canvas.getBoundingClientRect()
-  canvas.height = clientRect.height
-  canvas.width = clientRect.width
-}
+const maxlineWidth = 6
+const maxLineLength = 60
+const canvasPadding = maxLineLength + maxlineWidth / 2 + 1
 
-const lineSegment = useLineSegment(canvasRef)
-const drawFrame = (canvas: HTMLCanvasElement) => {
-  lineSegment.render()
-  requestAnimationFrame(() => drawFrame(canvas))
-}
+canvasRenderer.setRender((cxt, size) => {
+  zOffset.value += 0.001
+  cxt.clearRect(0, 0, size.width, size.height)
 
-watch([canvasRef], () => {
-  if (canvasRef.value != null) {
-    drawFrame(canvasRef.value)
-    resetSize(canvasRef.value)
+  for (let x = canvasPadding; x <= size.width - canvasPadding; x += 20) {
+    for (let y = canvasPadding; y <= size.height - canvasPadding; y += 20) {
+      const angle = angleNoise(x * 0.0005, y * 0.0005, zOffset.value) * 5
+      const color = colorNoise(x * 0.0005, y * 0.0005, zOffset.value) * 360
+      const alpha = angle
+
+      const toX = x + Math.cos(angle) * maxLineLength
+      const toY = y + Math.sin(angle) * maxLineLength
+
+      const linearGradient = cxt.createLinearGradient(x, y, toX, toY)
+      linearGradient.addColorStop(1, `hsla(${color}, 50%,50%, 1)`)
+      linearGradient.addColorStop(0, 'hsla(0,0%,0%,0)')
+      cxt.strokeStyle = linearGradient
+
+      cxt.lineWidth = 6
+      cxt.lineCap = 'round'
+
+      cxt.beginPath()
+      cxt.moveTo(x, y)
+      cxt.lineTo(toX, toY)
+      cxt.stroke()
+    }
   }
 })
-
 </script>
 <style lang="less" scoped>
 .screen-protect,
 .canvas {
-  width: 40rem;
-  height: 24rem;
+  width: 50rem;
+  height: 30rem;
   overflow: hidden;
-  background: white;
   border-radius: 10px;
 }
 </style>
