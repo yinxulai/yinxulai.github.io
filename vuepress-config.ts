bcd9f2fd9a2d7a03c608a3e96e85892c2bd9ecad
yinxulai <yinxulai@hotmail.com>
2021-09-07 10:06:27 +0800

    删除没用的 public 目录

--- vuepress-config.ts
+++ vuepress-config.ts
@@ -45,7 +45,6 @@ export default defineUserConfig<DefaultThemeOptions>({
   title: 'Alain Blog',
   description: 'Alain Blog',
   theme: path.resolve(__dirname, 'theme'),
-  public: path.resolve(__dirname, 'public'),
   dest: path.resolve(__dirname, '.output/dest'),
   temp: path.resolve(__dirname, '.output/temp'),
   cache: path.resolve(__dirname, '.output/cache'),
