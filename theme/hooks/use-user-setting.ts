1b5489f5f3f6af32616cb9dc55d3874fb20af70e
yinxulai <yinxulai@hotmail.com>
2021-09-28 18:39:31 +0800

    添加更多的坑

--- theme/hooks/use-user-setting.ts
+++ theme/hooks/use-user-setting.ts
@@ -1,5 +1,14 @@
-// 添加 command+. 显示开发模式的功能
+import { ref } from 'vue'
 
-export function useUserSetting() {
-  
+interface UserSetting {
+  // 添加 command+. 显示开发模式的功能
+  showHidden: boolean // 是否展示隐藏项目
+}
+
+export function useUserSetting(): UserSetting | null {
+  const setting = ref<UserSetting | null>(null)
+
+  const userSetting = localStorage.getItem('setting')
+  if (userSetting == null || userSetting === '') return setting.value
+  return null
 }
