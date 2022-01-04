599b3db2a490a12908edd9a760a4ec9e489e3ce1
Alain <yinxulai@hotmail.com>
2022-01-04 22:24:14 +0800

    添加文章：基于 web 的声波测距

---
+++ source/study/2021/12/28/sound-wave-measure-distance.vue
@@ -0,0 +1,13 @@
+<template>
+  <canvas id="canvas"></canvas>
+</template>
+<script lang="ts" setup>
+
+async function getSoundInput() {
+  const userMediaStream = await navigator.mediaDevices.getUserMedia({audio: true})
+  const audioNode = new AudioContext().createMediaStreamSource(userMediaStream)
+  audioNode.numberOfOutputs
+  // audioNode.addEventListener('') 
+}
+getSoundInput()
+</script>
---
+++ source/study/2021/12/28/基于 WEB 实现声波测距.md
@@ -0,0 +1,7 @@
+---
+lang: zh-CN
+title: 基于 WEB 实现声波测距
+description: 基于 WEB 实现声波测距
+---
+
+<sound-wave-measure-distance />
