08c3856c925c79687162ffeb59e982cd6309edbe
yinxulai <yinxulai@hotmail.com>
2023-12-14 14:18:54 +0800

    tailwindcss

--- index.tsx
+++ index.tsx
@@ -1,11 +1,7 @@
 import * as airx from "airx"
-
-// @ts-expect-error
 import pages from "~airx-pages"
 import { ThemeApp } from './theme'
 
 airx
   .createApp(<ThemeApp posts={pages} />)
   .mount(document.getElementById('app')!)
-
-console.log('22222')
